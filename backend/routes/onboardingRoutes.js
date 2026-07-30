bf3e0c31e18528f70d3828b0d92b11d9c6e2ca91
dev9597 <devanshusaini95@gmail.com>
2026-07-30 11:07:23 +0530

    Fix bcryptjs import to use bcrypt instead

--- backend/routes/onboardingRoutes.js
+++ backend/routes/onboardingRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { verifyToken } = require('../middleware/authMiddleware');
 const { requireRole, requireOwner, requireAdmin, requireRecruiterOrAbove, checkPlanLimit } = require('../middleware/rbacMiddleware');
